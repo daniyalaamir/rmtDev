2552d490ba222bc2661d3b0189014a89a75bfd80
Daniyal Aamir <daamir89@gmail.com>
2024-06-28 23:16:31 -0400

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import Background from "./Background";
+import Header from "./Header";
+import Container from "./Container";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <>
+      <Background />
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  )
+}
+
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          © Copyright by{" "}
+          Daniyal A. Intended for personal portfolio.
+        </p>
+      </small>
+      <p>
+        <span className="u-bold">109573</span> total jobs available
+      </p>
+    </footer>
+  );
+}
