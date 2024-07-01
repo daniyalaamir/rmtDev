891da8832e6657aaa16c4f8a77cd8a3c34b97dd0
Daniyal Aamir <daamir89@gmail.com>
2024-06-30 22:02:11 -0400

    add pagination

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,14 +14,27 @@ import SortingControls from "./SortingControls";
 import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
 import { Toaster } from "react-hot-toast";
+import { RESULTS_PER_PAGE } from "../lib/constants";
 
 export default function App() {
   const [searchText, setSearchText] = useState("")
   const debouncedSearchText = useDebounce(searchText, 250)
   const { jobItems, isLoading } = useJobItems(debouncedSearchText)
+  const [currentPage, setCurrentPage] = useState(1)
   
   const totalNumberOfResults = jobItems?.length || 0
-  const jobitemsSliced = jobItems?.slice(0, 7) || []
+  const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE
+  const jobitemsSliced = jobItems?.slice(
+    currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE, 
+    currentPage * RESULTS_PER_PAGE) || []
+
+  const handleChangePage = (direction: 'next' | 'previous') => {
+    if (direction === 'next') {
+      setCurrentPage(prev => prev + 1)
+    } else if (direction === 'previous') {
+      setCurrentPage(prev => prev - 1)
+    }
+  }
 
   return (
     <>
@@ -40,7 +53,11 @@ export default function App() {
             <SortingControls />
           </SidebarTop>
           <JobList jobItems={jobitemsSliced} isLoading={isLoading} />
-          <PaginationControls />
+          <PaginationControls 
+            currentPage={currentPage} 
+            totalNumberOfPages={totalNumberOfPages}
+            onClick={handleChangePage} 
+          />
         </Sidebar>
         <JobItemContent />
       </Container>
